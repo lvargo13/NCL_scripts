63af43243291f02aeeed4fe885f11bcbf9ef2678
Lauren Vargo <lvargo@lanl.gov>
2015-07-14 14:37:36 -0600

    updating the plot titles

--- run_DiagnosticPlots.sh
+++ run_DiagnosticPlots.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export filestart=90 
-export fileend=130 
+export filestart=170 
+export fileend=200
 export dataF="Greenland_5km_v1.1_SacksRev_c110629.nc" 
 export modelPreName="BG_CISM1_parallel_updating.cism.h."
 export modelPostName="-01-01-00000.nc"
