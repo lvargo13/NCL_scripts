bdc4fc8409c9dcf9324fb16ffe46495dcecbd7a3
Lauren Vargo <lvargo@lanl.gov>
2015-07-28 16:19:15 -0600

    Added edits from Jeremy and added instructions for running

--- find_CISMTest_files.sh
+++ find_CISMTest_files.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# INSTRUCTIONS FOR RUNNING THE SCRIPT: 
+# Configuration parameter combinations can be narrowed down within the 'grep command string' (lines 37-44)
+# To narrow down configurations, either 1) remove the string of the parameter you do not want to search or 2) set both strings to the parameter you do want to search
+# For example, to only see configurations in which evolution=3, the line would either be:
+# | xargs grep -l 'evolution=3' \  
+# or 
+# | xargs grep -l 'evolution=3\|evolution=3' \
+#
+# The first line of the grep string should follow this style: egrep -l 'parameter1=a|parameter1=b' "$RunPath"/runscript_CISM_config_test_*.sh \
+# The following lines should follow this stlye: | xargs grep -l 'parameter2=a\|parameter2=b' \
+
+# PARAMETER OPTIONS: 
 # which_ho_babc options: 4 5 
 # which_ho_approx options: 2 3 4
 # which_ho_gradient options: 0 1 
@@ -10,16 +22,19 @@
 #		      /glade/u/home/sprice/greenland_4km_ICs/sliding/greenland_4km_2015_03_12.mcb.nofloat.4310a.beta7.nc
 # evolution options: 3 4
 # which_ho_assemble_beta options: 0 1
-	
-# one option/variable: | xargs grep -l 'variable_string' \
-# multiple options/variable: | xargs grep -l 'which_ho_babc=4\|which_ho_babc=5' \
 
-# Define colors 
-normal=$(tput sgr0)
-red=$(tput setaf 1)
-green=$(tput setaf 2)
 
-egrep -l 'which_ho_babc=4|which_ho_babc=5' *.sh \
+
+# Allow script to be run from anywhere
+RunPath=/glade/u/home/lvargo/CISM_tests/IG_test_ensemble_setup
+
+# Remove old list_CISMTest_files.sh if it exists 
+if [ -f list_CISMTest_files.sh ]; then
+  rm list_CISMTest_files.sh
+fi
+
+# grep command string  
+ egrep -l 'which_ho_babc=4|which_ho_babc=5' "$RunPath"/runscript_CISM_config_test_*.sh \
 | xargs grep -l 'which_ho_approx=2\|which_ho_approx=3\|which_ho_approx=4' \
 | xargs grep -l 'which_ho_gradient=0\|which_ho_gradient=1' \
 | xargs grep -l 'which_ho_gradient_margin=0\|which_ho_gradient_margin=1\|which_ho_gradient_margin=2' \
@@ -27,15 +42,15 @@ egrep -l 'which_ho_babc=4|which_ho_babc=5' *.sh \
 | xargs grep -l 'greenland_4km.glissade.10kyr.beta6.SSacab_c140919.nc\|greenland_4km_2015_03_12.mcb.nofloat.nc\|greenland_4km_2015_03_12.mcb.nofloat.4310a.beta7.nc' \
 | xargs grep -l 'evolution=3\|evolution=4' \
 | xargs grep -l 'which_ho_assemble_beta=0\|which_ho_assemble_beta=1' > list_CISMTest_files.sh
-
-
- # Remove this file name from list_CISMTest_files.sh 
- sed -i '/find_CISMTest_files.sh/d' list_CISMTest_files.sh 
  
- # Extract only # from list_CISMTest_files.sh
- sed -i s/runscript_CISM_config_test_// list_CISMTest_files.sh
+ # Extract only the configuration # from list_CISMTest_files.sh
+ sed -i s?"$RunPath"/runscript_CISM_config_test_?? list_CISMTest_files.sh
  sed -i s/.sh// list_CISMTest_files.sh
 
+ # Define colors 
+ normal=$(tput sgr0)
+ red=$(tput setaf 1)
+ green=$(tput setaf 2)
 
 while read File_num; do
 
