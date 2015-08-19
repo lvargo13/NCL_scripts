4bde23187c07480c6c475ebe6bfe1d0a278bab05
Lauren Vargo <lvargo@lanl.gov>
2015-08-19 15:40:51 -0600

    Edited the Compare_CISM scripts to add files from different directories

--- run_Compare_CISM_plots.sh
+++ run_Compare_CISM_plots.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-export filestart=2 
-export fileend=2
-export modelAPreName="IG_CISM2_test_001.cism.h."
+export filestart=35 
+export fileend=65
+export FileAname="gradient=0"
+export FileBname="gradient=1"
+export modelAPreName="/glade/u/home/lvargo/scratch/archive/IG_CISM2_test_041/glc/hist/IG_CISM2_test_041.cism.h."
 export modelAPostName="-01-01-00000.nc"
-export modelBPreName="IG_CISM2_test_409.cism.h."
+export modelBPreName="/glade/u/home/lvargo/scratch/archive/IG_CISM2_test_113/glc/hist/IG_CISM2_test_113.cism.h."
 export modelBPostName="-01-01-00000.nc"
 export latlon="lat_lon.nc"
 ncl Compare_CISM_plots.ncl
