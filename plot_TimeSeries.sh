b5e035a19d418fc6f244cdd880a2a683573a4548
Lauren Vargo <lvargo@lanl.gov>
2015-10-27 07:33:45 -0600

    Bash script to 1) pull out ice volume and area and 2) call ncl script to plot

--- plot_TimeSeries.sh
+++ plot_TimeSeries.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# bash script: 1. pulls out ice area and ice volume and saves to .sh file
+# 2. calls plot_TimeSeries.ncl to plot the data
+
 # Area
 grep -h -m 1 'Total ice area*' glc.log.* > Area_TS_data.sh
 
@@ -10,3 +13,7 @@ sed -i 's/* Total ice area (km^2)//' Area_TS_data.sh
 grep -h -m 1 'Total ice volume*' glc.log.* > Vol_TS_data.sh
 
 sed -i 's/* Total ice volume (km^3)//' Vol_TS_data.sh   
+
+
+ncl plot_TimeSeries.ncl
+
