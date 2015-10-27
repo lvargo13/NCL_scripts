377949210527783507dfe5165feea090b50a5772
Lauren Vargo <lvargo@lanl.gov>
2015-10-27 07:25:15 -0600

    Run diagnostic plot ncl script (DiagnosticPlots.ncl)

--- run_DiagnosticPlots.sh
+++ run_DiagnosticPlots.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 # File to run the diagnostic plot package (DiagnosticPlots.ncl)
+# Compares CISM with data
+
 export filestart=170 
 export fileend=200
 export dataF="Greenland_5km_v1.1_SacksRev_c110629.nc" 
